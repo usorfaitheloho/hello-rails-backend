e33457024eb2627310f997fd13cc32bf5b4f9ca5
usorfaitheloho <usorfaitheloho@gmail.com>
2022-07-30 18:43:29 -0700

    Setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  # namespace :v1, defaults: { format: 'json' } do
-  #   get 'greetings', to: 'greetings#index'
-  # end
+  namespace :v1, defaults: { format: 'json' } do
+    get 'greetings', to: 'greetings#index'
+  end
 
   get '*page', to: 'static#index', constraints: ->(req) do
     !req.xhr? && req.format.html?
